b7048426e72c60c768de13e3df8f6668f33240c1
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 10:39:21 -0500

    finished slide 5 finally

---
+++ nursingHomeReg/addinfo.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Additional Information of Patient</title>
+    </head>
+    <h1>Additional Information of Patient</h1>
+    <form action="" method="POST">
+        <br><label>Patient ID</label><input type="text" name="Patient ID" /><br>
+        <br><label>Group</label><input type="text" name="Group" /><br>
+        <br> <label>Admission Date</label><input type="text" name="Admission Date" /><br>
+        <br> <label>Date</label><input type="text" name="Date" /><br>
+        <br> <label>Doctor</label>  <select name="DOCTORDROPDOWN"><br>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+  </select>
+  <br><br>
+  <input type="submit">
+
+    </form>
+
+    
