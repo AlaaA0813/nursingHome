eff778b803cb192f311dded64bf69cde3dfbd137
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 17:36:17 -0500

    create payment view

---
+++ nursingHomeReg/payment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Payment</title>
+    </head>
+    <h1>Payment</h1>
+    <form action="" method="POST">
+        <br><label>Patient ID</label><input type="text" name="Patient ID" /><br>
+        <br><label>Total Due</label><input type="text" name="Total Due" /><br>
+        <br> <label>New Payment</label><input type="text" name="New Payment" /><br>
+         <ul>
+            <li>$10 for every day</li>
+            <li>$50 for every appointment</li>
+            <li>$5 for every medicine/month</li>
+        </ul>
+  <br><input type="submit" name="OK" value="OK">
+  <input type="submit" name="cancel" value="Cancel">
+
+</form>
+
+    
