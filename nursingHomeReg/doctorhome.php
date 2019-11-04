f8fd6bb4175007b1c50c09584267c069e65f8520
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 16:40:44 -0500

    create doctor's  home view

---
+++ nursingHomeReg/doctorhome.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Doctor's Home</title>
+    </head>
+    <body>
+        <h1>Doctor's Home</h1>
+        <p>Add a search option for each attribute</p>
+        <form action="" method="POST">
+            <table>
+            <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Comment</th>
+                <th>Morning Med</th>
+                <th>Afternoon Med</th>
+                <th>Night Med</th>
+            </tr>
+            </table>
+            <label>Appointment</label>
+            <input type="text" name="Till Date" />
+            <table>
+            <tr>
+                <th>Patient</th>
+                <th>Date</th>
+            </tr>
+            </table>
+           
+        </form>
+    </body>
+</html>
