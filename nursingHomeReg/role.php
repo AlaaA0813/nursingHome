aa540ab3a2663fe1d7911530b12a01f23f645464
Alaa Ayyash <aja5063@gmail.com>
2019-11-04 10:15:08 -0500

    create role html

---
+++ nursingHomeReg/role.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Roles</title>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <h2>Roles</h2>
+            <label>New Role: </label><input type="text" name="newrole" /><br>
+            <label>Access Level: </label><input type="text" name="accesslevel" /><br>
+            <input type="submit" name="addrole" value="Add New Role">
+            <input type="submit" name="cancel" value="Cancel">
+        </form>
+    </body>
+</html>
