36d55f65f280f21f55a4f04ebcb8e57c1fbbbf4d
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 16:54:38 -0500

    create patient of doctor view

---
+++ nursingHomeReg/patientofdoc.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Patient of Doctor</title>
+    </head>
+    <body>
+        <h1>Patient of Doctor</h1>
+        <form action="" method="POST">
+            <table>
+            <tr>
+                <th>Date</th>
+                <th>Comment</th>
+                <th>Morning Med</th>
+                <th>Afternoon Med</th>
+                <th>Night Med</th>
+            </tr>
+            </table>
+            <label>New Prescription</label>
+            <table>
+            <tr>
+                <th>Comment</th>
+                <th>Morning Med</th>
+                <th>Afternoon Med</th>
+                <th>Night Med</th>
+            </tr>
+            </table>
+            <br><input type="submit" name="OK" value="OK">
+            <input type="submit" name="cancel" value="Cancel">  
+        </form>
+    </body>
+</html>
