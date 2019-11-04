754ac3b8b209c132ce4127474a71a87af249e2ec
Alaa Ayyash <aja5063@gmail.com>
2019-11-04 10:54:31 -0500

    fill patient.php with html and table structure

---
+++ nursingHomeReg/patients.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Patients</title>
+    </head>
+    <body>
+        <h1>Patients Chart</h1>
+        <h2>Add a search option for each attribute.</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Emergency Contact Name</th>
+                <th>Emergency Contact Phone Number</th>
+                <th>Admission Date</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+</html>
