ee31d3f4aebd95305f6a9d772620bb5b6741db3b
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 17:30:56 -0500

    create admin report's view

---
+++ nursingHomeReg/adminreport.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Admin's Report</title>
+    </head>
+    <body>
+        <h1>Admin's Report</h1>
+        <form action="" method="POST">
+        <label>Date: </label><input type="text" name="Date" /><br>
+        <input type="submit" name="missedpatientactivity" value="Missed Patient Activity">    
+            <table>
+            <tr>
+                <th>Patient's Name</th>
+                <th>Doctor's Name</th>
+                <th>Doctor's Appointment</th>
+                <th>Caregiver's Name</th>
+                <th>Morning Medicine</th>
+                <th>Night Medicine</th>
+                <th>Breakfast</th>
+                <th>Lunch</th>
+                <th>Dinner</th>
+            </tr>
+            </table>
+            <h1>ADD CHECK BOXES</h1>
+        </form>
+    </body>
+</html>
