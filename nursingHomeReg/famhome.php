c30be3de42efc8beea0969b6dbc09f12d0a09fb6
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-04 17:26:04 -0500

    create family member home view

---
+++ nursingHomeReg/famhome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Family Member's Home</title>
+    </head>
+    <body>
+        <h1>Family Member's Home</h1>
+        <form action="" method="POST">
+        <label>Date: </label><input type="text" name="Date" /><br>
+            <label>Family Code: </label><input type="text" name="Family Code" /><br>
+            <label>Patient ID: </label><input type="text" name="Patient ID" /><br>
+            <table>
+            <tr>
+                <th>Doctor's Name</th>
+                <th>Doctor's Appointment</th>
+                <th>Caregiver's Name</th>
+                <th>Morning Medicine</th>
+                <th>Night Medicine</th>
+                <th>Breakfast</th>
+                <th>Lunch</th>
+                <th>Dinner</th>
+            </tr>
+            </table>
+            <h1>ADD CHECK BOXES</h1>
+        </form>
+    </body>
+</html>
