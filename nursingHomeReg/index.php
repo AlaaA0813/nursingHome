37c61b99acac9e8d1a66f60515fa794db3a9bda3
Alaa Ayyash <aja5063@gmail.com>
2019-11-04 09:06:39 -0500

    create homepage html and login html

---
+++ nursingHomeReg/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Home Page</title>
+    </head>
+
+    <h1>Nursing Home Home Page</h1>
+    <h2>Log in to an existing account or register as a new user.</h2>
+
+    <form action="">
+        <input type="submit" value="Go To Log In" />
+    </form>
+    <form action="">
+        <input type="submit" value="Go To Sign Up!" />
+    </form>
+</html>
---
+++ nursingHomeReg/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Log In</title>
+    </head>
+
+    <h1>Login In</h1>
+    <h2>Please provide account information.</h2>
+
+    <form action="" method="POST">
+        <label>Email</label><input type="text" name="email" />
+        <label>Password</label><input type="text" name="password" />
+        <input type="submit" name="logIn" value="Log In" />
+        <input type="submit" name="cancel" value="Cancel" />
+    </form>
+</html>
