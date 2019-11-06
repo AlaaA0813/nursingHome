a74d8cbf740a2364dffee623c742e2a1700faecf
Alaa Ayyash <aja5063@gmail.com>
2019-11-05 19:20:10 -0500

    create db.php to connect files to db

---
+++ nursingHomeReg/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "nursingHome";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+
+?>
