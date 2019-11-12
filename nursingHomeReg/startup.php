65e3841e56581202e55795117699c82545f031d4
Calista Alicea <cealicea171@stevenscollege.edu>
2019-11-11 20:22:52 -0500

    create start up page, link for register and login page, start working on nav

---
+++ nursingHomeReg/startup.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8">
+        <title>Start Up Page</title>
+    </head>
+    <body>
+    <a href="registration.php">Register</a>
+    <a href="login.php">Login</a>
+    </body>
+    </html>
